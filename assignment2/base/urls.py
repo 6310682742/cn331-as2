acb488da2b7c366ac056cb43f9cd1099c2f30274
Chaiwat Klanrueangsaeng <6310682742@cn331>
2022-09-16 16:19:54 +0700

    Add user login logout regist

--- assignment2/base/urls.py
+++ assignment2/base/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login', views.login, name='login')
+    path('login/', views.loginUser, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('regist/', views.registUser, name='regist'),
 ]
